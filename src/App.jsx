22faaeb179d095143afc3dff0b77fa610981fc6e
emrd434 <emrd434@gmail.com>
2024-09-07 10:17:32 +0000

    first commi

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+
+function App() {
+  const [login, setLogin] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  const handleLogin = () => {
+    setLogin(true);
+    setTimer(5);
+
+    document.getElementById("loginb").style.visibility = "hidden";
+
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+
+    const id = setInterval(() => {
+      setTimer(prevTimer => {
+        if (prevTimer < 1) {
+          clearInterval(id); // Clear the interval
+          setLogin(false); // Update login status
+          document.getElementById("loginb").style.visibility = "visible";
+          toast.error("Session Expired");
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+
+    setIntervalId(id);
+  };
+
+  return (
+    <>
+      <h1 id="loginlabel">
+        {login ? "Logged in" : "Not logged in"}
+      </h1>
+      <button onClick={handleLogin} id="loginb">
+        Login
+      </button>
+      <p>{login ? `Time remaining: ${timer}s` : ''}</p>
+    </>
+  );
+}
+
+export default App;
